b5e821c3a792e9e41e2ef5f58c09a83dbb039612
Robert Sherman <122405581+rsherman1@users.noreply.github.com>
2023-02-19 04:28:15 +0000

    changed animation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Check JS is Connected
 console.log('JS Connected');
 
-gsap.to("#doodle", {duration: 1, x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45});
+gsap.to("#doodle", {duration: 1, x: 100, y: 100, scale: 2);
